94725b2a1787e8ba7cff5f3d524a1b77bc86bb04
Anny <ttaminh03@gmail.com>
2026-06-27 16:44:34 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+let VALUE = 0
+let PASSWORD_INPUT = ""
+let PASSWORD = ""
+input.onButtonPressed(Button.A, function () {
+    VALUE += 1
+    basic.showNumber(VALUE)
+})
+input.onButtonPressed(Button.AB, function () {
+    PASSWORD_INPUT = "" + PASSWORD_INPUT + convertToText(VALUE)
+    VALUE = 0
+})
+input.onButtonPressed(Button.B, function () {
+    VALUE += -1
+    basic.showNumber(VALUE)
+})
+basic.forever(function () {
+    PASSWORD = "1234"
+    PASSWORD_INPUT = ""
+    VALUE = 0
+})
+basic.forever(function () {
+    if (PASSWORD_INPUT == PASSWORD) {
+        basic.showString("C'mon in")
+    } else {
+        basic.showString("GET OUT!")
+    }
+})
